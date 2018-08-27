8a6fc71c4c5335c291b2e3a9b7b2aa3e64322fa4
SirBokeye <42724693+SirBokeye@users.noreply.github.com>
2018-08-27 03:48:30 +0200

    Update Bixby2.py

--- Bixby2.py
+++ Bixby2.py
@@ -20,34 +20,7 @@ async def on_message(message):
     if message.content.startswith('!cookie'):
 	msg = 'Lekker een sappig koekje'.format(message)
         await client.send_message(message.channel, ":cookie:")
-        
-    if message.content == "BOK.":
-        await client.send_message(message.channel, ":goat:") 
 
-    if message.content == "Bixby":
-        await client.send_message(message.channel, "BIXBY!") 
-
-    if message.content == "BIXBY":
-        await client.send_message(message.channel, "BIXBY FTW!")
-
-    if message.content == "Fuck off":
-        await client.send_message(message.channel, "Zuig een piemel!")
-
-    if message.content == "Homo":
-        await client.send_message(message.channel, "Kijk naar jezelf!")
-        
-    if message.content == "Ga weg":
-        await client.send_message(message.channel, "Niet lief!")
-
-    if message.content == "Zuig een piemel":
-        await client.send_message(message.channel, "Nee gadver!")
-
-    if message.content == "Zuig een lul":
-        await client.send_message(message.channel, "Ga dat lekker zelf doen viezerik!")
-
-    if message.content.startswith('!Hi'):
-        userID = message.author.id
-        await client.send_message(message.channel, "<@%s> Hi!" % (userID))
         
 @client.command(pass_context=True)
 async def join(ctx):
