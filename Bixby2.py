e4bd5a967fbea4b3193a872851ce3011a84c7d1d
SirBokeye <42724693+SirBokeye@users.noreply.github.com>
2018-08-27 12:18:33 +0200

    Update Bixby2.py

--- Bixby2.py
+++ Bixby2.py
@@ -48,6 +48,24 @@ async def on_message(message):
     if message.content.startswith('!Hi'):
         userID = message.author.id
         await client.send_message(message.channel, "<@%s> Hi!" % (userID))
-        
+    
+client.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
+
+client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+    
+client.command(pass_context=True)
+async def play(ctx, url):
+	server = ctx.message.server
+	voice_client = client.voice_client_in(server)
+	player = await voice_client.create_ytdl_player(url)
+	players[server.id] = player
+	player.start()
 
 client.run(os.getenv('TOKEN'))
